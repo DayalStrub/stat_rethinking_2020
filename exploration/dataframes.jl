369b1bcfe959da7306ad9204d82c4d041617471c
DayalStrub <strubdayal@gmail.com>
2021-12-04 17:05:41 +0000

    add gitignore, project.toml, etc.

---
+++ exploration/dataframes.jl
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .jl
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.1
+#   kernelspec:
+#     display_name: Julia 1.6.4
+#     language: julia
+#     name: julia-1.6
+# ---
+
+using DataFrames
+
+
