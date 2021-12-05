285ba875ce69796e06263f2cefc58de7db2cf46c
DayalStrub <strubdayal@gmail.com>
2021-12-05 17:22:45 +0000

    add csv, update exploration

--- exploration/dataframes.jl
+++ exploration/dataframes.jl
@@ -12,6 +12,19 @@
 #     name: julia-1.6
 # ---
 
+# https://www.ahsmart.com/assets/pages/data-wrangling-with-data-frames-jl-cheat-sheet/DataFramesCheatSheet_v1.x_rev1.pdf
+
+using CSV
 using DataFrames
+
+df = CSV.read("../data/Howell1.csv", DataFrame);
+
+first(df, 5)
+
+# +
+# describe(df)
+# -
+
+nrow(df)
 
 
